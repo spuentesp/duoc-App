941e4ebe1d6d01b410975fe53ba90a6301ff6bf8
spuentesp <spuentesp@gmail.com>
2025-10-09 11:00:42 -0300

    clase 3 - incompleta

---
+++ app/src/main/java/com/example/app/model/Pokemon.kt
@@ -0,0 +1,41 @@
+package com.example.app.model
+
+
+import SerializedName
+
+
+data class Pokemon(val id: Int,
+                   val name: String,
+                   val sprites: PokemonSprites,
+                   val types: List<PokemonTypeEntry>,
+                   val stats: List<PokemonStatEntry>)
+
+
+data class PokemonSprites(
+    // @serializedname se usa si el nombre del campo en json es diferente al de la variable
+    @SerializedName("front_default")
+    val frontDefault: String
+)
+
+data class PokemonTypeEntry(
+    var slot: Int,
+    val Type: PokemonType
+)
+
+data class PokemonType(
+    val name: String
+)
+
+data class PokemonStatEntry(
+    @SerializedName("base_stat")
+    val baseStat: Int,
+    val stat: PokemonStat
+)
+
+data class PokemonStat(
+    val name:String
+)
+
+
+
+
